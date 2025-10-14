defcb027fe8520d951628377ba79adc2d12761f5
{ppbotelhoDev} <{ppbotelho08@gmail.com}>
2025-10-14 10:54:34 -0300

    Teacher page - Structure fineshed

--- src/pages/DashboardProf.jsx
+++ src/pages/DashboardProf.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from "react-router-dom";
+import { LogOut } from "lucide-react";
 
 const DashboardProf = () => {
   //Recebe os dados vindo o PageLogin
@@ -8,67 +9,169 @@ const DashboardProf = () => {
   return (
     <div>
       <header className="header-Prof">
-        <div className="containerHeader">
+        <div className="containerHeader container">
           <div className="divLogo">
-            <img className="logo" src="logo.png" alt="" />
+            <a href="">
+              <img className="logo" src="logo.png" alt="" />
+            </a>
           </div>
           <div className="navbar">
             <div className="linksNav">
-              <a className="linkNav" href="">Home</a>
-              <a className="linkNav" href="">Dashboard</a>
-              <a className="linkNav" href="">Notas</a>
-              <a className="linkNav" href="">Cadastrar</a>
+              <a className="linkNav" href="">
+                Home
+              </a>
+              <a className="linkNav" href="">
+                Dashboard
+              </a>
+              <a className="linkNav" href="">
+                Notas
+              </a>
+              <a className="linkNav" href="">
+                Cadastrar
+              </a>
             </div>
             <div className="itensUser">
               <div className="newQuiz">
-                <a href=""><button className="btnQuiz">Criar novo quiz +</button></a>
+                <a href="">
+                  <button className="btnQuiz">Criar novo quiz +</button>
+                </a>
               </div>
               <div className="perfilUser">
-                <p className="paragrafoUser">Olá professor,<br/>{profLogado.nome}</p>
+                <p className="paragrafoUser">
+                  Olá professor,
+                  <br />
+                  {profLogado.nome}
+                </p>
               </div>
               <div className="userExit">
-                <a href=""><img className="imgLogout" src="logout.png" alt="" /></a>
+                <a className="iconExit" href="">
+                  <LogOut />
+                </a>
               </div>
             </div>
           </div>
         </div>
       </header>
-      {/* <main>
-        <div>
-          <h1></h1>
+      <main>
+        <div className="containerMain container">
+          <div className="methrics-Painel">
+            <div className="methrics">
+              <div className="methrics-Title">
+                <h3>Total de Quizes</h3>
+              </div>
+              <div className="methrics-Value">
+                <h1>{0}</h1>
+              </div>
+            </div>
+            <div className="methrics">
+              <div className="methrics-Title">
+                <h3>Média de acertos</h3>
+              </div>
+              <div className="methrics-Value">
+                <h1>{0}/10</h1>
+              </div>
+            </div>
+            <div className="methrics">
+              <div className="methrics-Title">
+                <h3>Quizes feitos</h3>
+              </div>
+              <div className="methrics-Value">
+                <h1>{0}</h1>
+              </div>
+            </div>
+          </div>
+          <div className="quiz-painel">
+            <div className="card-Quiz">
+              <div className="img-Quiz">
+                <img src="math.png" alt="" width={65} />
+              </div>
+              <div className="nome-Quiz">
+                <h4>Simulado Matemática #{2345}</h4>
+              </div>
+              <div className="legenda-Quiz">
+                <p>
+                  Simulador Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit.
+                </p>
+              </div>
+              <div className="btn-Acessar-Quiz">
+                <a href="">Acessar</a>
+              </div>
+            </div>
+
+            <div className="card-Quiz">
+              <div className="img-Quiz">
+                <img src="math.png" alt="" width={65} />
+              </div>
+              <div className="nome-Quiz">
+                <h4>Simulado Matemática #{2345}</h4>
+              </div>
+              <div className="legenda-Quiz">
+                <p>
+                  Simulador Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit.
+                </p>
+              </div>
+              <div className="btn-Acessar-Quiz">
+                <a href="">Acessar</a>
+              </div>
+            </div>
+
+            <div className="card-Quiz">
+              <div className="img-Quiz">
+                <img src="math.png" alt="" width={65} />
+              </div>
+              <div className="nome-Quiz">
+                <h4>Simulado Matemática #{2345}</h4>
+              </div>
+              <div className="legenda-Quiz">
+                <p>
+                  Simulador Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit.
+                </p>
+              </div>
+              <div className="btn-Acessar-Quiz">
+                <a href="">Acessar</a>
+              </div>
+            </div>
+
+            <div className="card-Quiz">
+              <div className="img-Quiz">
+                <img src="math.png" alt="" width={65} />
+              </div>
+              <div className="nome-Quiz">
+                <h4>Simulado Matemática #{2345}</h4>
+              </div>
+              <div className="legenda-Quiz">
+                <p>
+                  Simulador Lorem ipsum dolor sit amet consectetur adipisicing
+                  elit.
+                </p>
+              </div>
+              <div className="btn-Acessar-Quiz">
+                <a href="">Acessar</a>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="methrics-Painel">
-          <div className="methrics">
-            {" "}
-            <h1>Métrica 1</h1>{" "}
-          </div>
-          <div className="methrics">
-            {" "}
-            <h1>Métrica 2</h1>{" "}
-          </div>
-          <div className="methrics">
-            {" "}
-            <h1>Métrica 3</h1>{" "}
-          </div>
-        </div>
-        <div className="quiz-painel">{/* <CardQuiz/> }</div>
       </main>
       <footer>
-        <div>Atalhos</div>
-        <div>
-          <div>Data</div>
-          <div>
-            Powered by{" "}
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https//:www.linkedin/in/pedropbotelho "
-            >
-              ppbotelho_
-            </a>
+        <div className="container container-footer">
+          <div className="footer-top">
+            <a href="">Suporte</a>
+            <a href="">Política de Privacidade</a>
+            <a href="">Política de reembolso</a>
+            <a href="">Termos de serviço</a>
+            <a href="">Dashboard</a>
+            <a href="">Criar simulado</a>
+            <a href="">Cadastrar aluno</a>
+            <a href="">Notas</a>
+          </div>
+          <div className="footer-bottom">
+            <p>&#169;2025 Expert Vest</p>
           </div>
         </div>
-      </footer> */}
+      </footer>
     </div>
   );
 };
